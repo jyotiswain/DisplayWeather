40e76b9eb60a39967419987e1df4fec346936ffa
JyotiSwain <>
2022-02-12 23:56:09 +0530

    Version 2

--- src/App.js
+++ src/App.js
@@ -30,7 +30,7 @@ const App = () => {
                         <sup>&deg;C</sup>
                     </div>
                     <div className="info">
-                        <img className="city-icon" src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`} alt={weather.weather[0].description} />
+                        <img className="city-icon" src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`} alt={weather.weather[0].description} />
                         <p>{weather.weather[0].description}</p>
                     </div>
                 </div>
